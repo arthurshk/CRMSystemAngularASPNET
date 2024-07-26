c8fbb5299b05aa91eaff4571ba89ff7cf6ad5f5d
Arthur <arthurdragon39@gmail.com>
2024-07-26 19:10:03 -0400

    commit

---
+++ CRMSystemAngularASPNET.Server/Controllers/AccountsController.cs
@@ -0,0 +1,69 @@
+﻿using CRMSystemAngularASPNET.Server.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CRMSystemAngularASPNET.Server.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AccountsController : ControllerBase
+    {
+        private readonly CrmContext _context;
+
+        public AccountsController(CrmContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Account>>> GetAccounts()
+        {
+            return await _context.Accounts.ToListAsync();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Account>> CreateAccount(Account account)
+        {
+            _context.Accounts.Add(account);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction(nameof(GetAccount), new { id = account.Id }, account);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Account>> GetAccount(int id)
+        {
+            var account = await _context.Accounts.FindAsync(id);
+            if (account == null)
+            {
+                return NotFound();
+            }
+            return account;
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateAccount(int id, Account account)
+        {
+            if (id != account.Id)
+            {
+                return BadRequest();
+            }
+            _context.Entry(account).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteAccount(int id)
+        {
+            var account = await _context.Accounts.FindAsync(id);
+            if (account == null)
+            {
+                return NotFound();
+            }
+            _context.Accounts.Remove(account);
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+    }
+
+}
---
+++ CRMSystemAngularASPNET.Server/Models/CrmContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CRMSystemAngularASPNET.Server.Models
+{
+    public class CrmContext : DbContext
+    {
+        public CrmContext(DbContextOptions<CrmContext> options) : base(options)
+        { 
+        
+        }
+        public DbSet<Account> Accounts { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+    }
+}
