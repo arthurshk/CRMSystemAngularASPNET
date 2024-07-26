078786db5436dd7a1410350c9e58c825a0f4a3f2
Arthur <arthurdragon39@gmail.com>
2024-07-26 18:39:06 -0400

    Add project files.

---
+++ CRMSystemAngularASPNET.Server/Models/Account.cs
@@ -0,0 +1,9 @@
+﻿namespace CRMSystemAngularASPNET.Server.Models
+{
+    public class Account
+    {
+        public int Id {  get; set; }
+        public string Name { get; set; }
+        public string Industry { get; set; }
+    }
+}
---
+++ CRMSystemAngularASPNET.Server/Models/Contact.cs
@@ -0,0 +1,11 @@
+﻿namespace CRMSystemAngularASPNET.Server.Models
+{
+    public class Contact
+    {
+        public int Id { get; set; }
+        public string firstName {  get; set; }
+        public string lastName { get; set; }
+        public int accountId { get; set; }
+        public Account account { get; set; }
+    }
+}
