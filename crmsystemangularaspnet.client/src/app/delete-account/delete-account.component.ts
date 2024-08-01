805aceb127352984dcd86010e0e7576521649254
Arthur <arthurdragon39@gmail.com>
2024-08-01 10:29:47 -0400

    commit

--- crmsystemangularaspnet.client/src/app/delete-account/delete-account.component.ts
+++ crmsystemangularaspnet.client/src/app/delete-account/delete-account.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AccountService } from '../account.service';
 import { Account } from '../Models/Account';
 
@@ -7,14 +7,26 @@ import { Account } from '../Models/Account';
   templateUrl: './delete-account.component.html',
   styleUrls: ['./delete-account.component.css']
 })
-export class DeleteAccountComponent {
-  @Input() account: Account = { id: 0, name: '', industry: '', email: '' };
+export class DeleteAccountComponent implements OnInit {
+  accounts: Account[] = [];
 
   constructor(private accountService: AccountService) { }
 
-  deleteAccount(id: number) {
+  ngOnInit() {
+    this.accountService.getAccounts().subscribe(accounts => {
+      this.accounts = accounts;
+    });
+  }
+
+  deleteAccount(id: number | undefined) {
+    if (id === undefined) {
+      console.error('Account ID is undefined');
+      return;
+    }
+
     this.accountService.deleteAccount(id).subscribe(() => {
       console.log('Account deleted');
+      this.accounts = this.accounts.filter(account => account.id !== id);
     });
   }
 }
