34cdf16fd04e201b0e6d57e5dc9511ef1bc8091a
Arthur <arthurdragon39@gmail.com>
2024-08-03 20:27:37 -0400

    commit

--- crmsystemangularaspnet.client/src/app/delete-account/delete-account.component.ts
+++ crmsystemangularaspnet.client/src/app/delete-account/delete-account.component.ts
@@ -19,4 +19,8 @@ export class DeleteAccountComponent {
       this.router.navigate(['/list-accounts']);
     });
   }
+
+  public navigateToList() {
+    this.router.navigate(['/list-accounts']);
+  }
 }
