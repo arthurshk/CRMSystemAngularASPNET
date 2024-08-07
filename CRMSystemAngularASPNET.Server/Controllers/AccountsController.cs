49d0a4d1e22719b335c9e403c7212bf50bb288df
Arthur <arthurdragon39@gmail.com>
2024-08-07 12:29:13 -0400

    commit

--- CRMSystemAngularASPNET.Server/Controllers/AccountsController.cs
+++ CRMSystemAngularASPNET.Server/Controllers/AccountsController.cs
@@ -1,71 +1,40 @@
 ﻿using CRMSystemAngularASPNET.Server.Models;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using static System.Net.Mime.MediaTypeNames;
-using static System.Runtime.InteropServices.JavaScript.JSType;
-using System;
 
-namespace CRMSystemAngularASPNET.Server.Controllers
+[Route("api/[controller]")]
+[ApiController]
+public class AccountsController : ControllerBase
 {
-    [ApiController]
-    [Route("api/[controller]")]
-    public class AccountsController : ControllerBase
+    private readonly CrmContext _context;
+
+    public AccountsController(CrmContext context)
     {
-        private readonly CrmContext _context;
+        _context = context;
+    }
 
-        public AccountsController(CrmContext context)
+    [HttpPost]
+    public async Task<IActionResult> CreateAccount([FromBody] Account account)
+    {
+        if (account == null)
         {
-            _context = context;
+            return BadRequest();
         }
 
-        [HttpGet]
-        public async Task<ActionResult<IEnumerable<Account>>> GetAccounts()
+        _context.Accounts.Add(account);
+        await _context.SaveChangesAsync();
+
+        return CreatedAtAction(nameof(GetAccountById), new { id = account.Id }, account);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetAccountById(int id)
+    {
+        var account = await _context.Accounts.FindAsync(id);
+        if (account == null)
         {
-            return await _context.Accounts.ToListAsync();
+            return NotFound();
         }
 
-        [HttpPost]
-        public async Task<ActionResult<Account>> CreateAccount(Account account)
-        {
-            _context.Accounts.Add(account);
-            await _context.SaveChangesAsync();
-            return CreatedAtAction(nameof(GetAccount), new { id = account.Id }, account);
-        }
-
-        [HttpGet("{id}")]
-        public async Task<ActionResult<Account>> GetAccount(int id)
-        {
-            var account = await _context.Accounts.FindAsync(id);
-            if (account == null)
-            {
-                return NotFound();
-            }
-            return account;
-        }
-
-        [HttpPut("{id}")]
-        public async Task<IActionResult> UpdateAccount(int id, Account account)
-        {
-            if (id != account.Id)
-            {
-                return BadRequest();
-            }
-            _context.Entry(account).State = EntityState.Modified;
-            await _context.SaveChangesAsync();
-            return NoContent();
-        }
-
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> DeleteAccount(int id)
-        {
-            var account = await _context.Accounts.FindAsync(id);
-            if (account == null)
-            {
-                return NotFound();
-            }
-            _context.Accounts.Remove(account);
-            await _context.SaveChangesAsync();
-            return NoContent();
-        }
+        return Ok(account);
     }
 }
